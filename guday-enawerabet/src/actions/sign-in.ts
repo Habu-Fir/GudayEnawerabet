962ddae8fa9dd7f7302d4266e1bb3d733afc63c3
Habu-Fir <tohabtish16@gmail.com>
2024-08-08 14:35:56 +0300

    create a separation action folder for post,comment and topic

--- guday-enawerabet/src/actions/sign-in.ts
+++ guday-enawerabet/src/actions/sign-in.ts
@@ -6,6 +6,3 @@ export async function signIn() {
     return auth.signIn('github')
 }
 
-export async function signOut() {
-    return auth.signOut()
-}
--- guday-enawerabet/src/app/page.tsx
+++ guday-enawerabet/src/app/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@nextui-org/react";
 import * as actions from '@/actions'
 import { auth } from '@/auth'
 import Profile from "@/components/profile";
+import { signIn } from "@/actions/sign-in";
+import { signOut } from "@/actions/sign-out";
 
 export default async function Home() {
 
@@ -9,8 +11,8 @@ export default async function Home() {
 
   return (
     <div className="m-4">
-      <form action={actions.signIn}> <Button className="space-x-2" type="submit">SignIn</Button></form>
-      <form action={actions.signOut}> <Button className="space-x-2" type="submit">SignOut</Button></form>
+      <form action={signIn}> <Button className="space-x-2" type="submit">SignIn</Button></form>
+      <form action={signOut}> <Button className="space-x-2" type="submit">SignOut</Button></form>
       {session?.user ? <div>SignedIn</div> : <div>SignedOut</div>}
       <div>
         <Profile />
