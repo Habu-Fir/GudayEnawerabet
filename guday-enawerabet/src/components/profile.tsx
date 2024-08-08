14bd3c3f06742d1c7fa18def57bf0ed3a0268155
Habu-Fir <tohabtish16@gmail.com>
2024-08-08 14:06:48 +0300

    initial setup and sign in and signout functionality using github provider

---
+++ guday-enawerabet/src/components/profile.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
+
+export default function Profile() {
+
+    const session = useSession()
+    if (session.data?.user) {
+        return <div>From client the user: is SignedIn</div>
+    }
+    else {
+        return <div> from client the user: is not signedIN</div>
+    }
+
+
+}
